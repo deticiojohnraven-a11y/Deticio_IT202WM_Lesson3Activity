847fd10c2f9b8554fc72a988f36faec578e1173a
deticiojohnraven-a11y <deticiojohnraven@gmail.com>
2026-03-03 23:43:52 +0800

    Add files via upload

---
+++ Deticio_Act3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Escasinas_Act3
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Deticio_Act3.EnrollmentForm());
+        }
+    }
+}
